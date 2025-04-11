060d39e5629f4edae0ff81a3ed84ff199582d0f5
Colton <81594541+coltonk1@users.noreply.github.com>
2025-04-11 15:32:12 -0400

    main update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,36 @@
-import React from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Login from './pages/login.jsx';
-import Register from './pages/register.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
-import Home from './pages/home.jsx';
-import NotFound from './pages/NotFound.jsx';
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Home from "./pages/home.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function Logout() {
-  localStorage.clear();
-  return <Navigate to="/login" replace />;
+    localStorage.clear();
+    return <Navigate to="/login" replace />;
 }
 
 function RegisterAndLogout() {
-  localStorage.clear();
-  return <Register />
+    localStorage.clear();
+    return <Register />;
 }
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/register'
-          element={<RegisterAndLogout />}
-        />
-        <Route
-          path='/logout'
-          element={<Logout />}
-        />
-        <Route
-          path='*'
-          element={<NotFound />}
-        />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route index element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<RegisterAndLogout />} />
+                <Route path="/logout" element={<Logout />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
