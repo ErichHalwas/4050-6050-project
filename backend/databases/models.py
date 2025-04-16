4bfca999266c9a8d2a5932ddedfd0311c885363f
Joshua <joshua@dason.org>
2025-04-16 17:22:51 -0400

    Update models.py to add event info database

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -8,3 +8,23 @@ class user_info(models.Model):
     
     def __str__(self):
         return self.email
+        
+class event_info(models.Model):
+    #event description
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    host = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    
+    #date
+    start_time = models.CharField(max_length=20)
+    end_time = models.CharField(max_length=20)
+    date = models.CharField(max_length=40)
+    
+    #location
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=30)
+    zipcode = models.CharField(max_length=10)
+    latitude = models.CharField(max_length=10)
+    longitude = models.CharField(max_length=10)
