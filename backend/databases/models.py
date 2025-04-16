09f9fcb9979d2b38555b80ea55f2bcfce8892731
Joshua <joshua@dason.org>
2025-04-16 17:38:46 -0400

    Update models.py to include attending and time zone columns

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -15,16 +15,19 @@ class event_info(models.Model):
     description = models.CharField(max_length=500)
     host = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
+    attending = models.IntegerField(max_length = 10)
     
     #date
     start_time = models.CharField(max_length=20)
     end_time = models.CharField(max_length=20)
+    time_zone = models.CharField(max_length = 35)
     date = models.CharField(max_length=40)
     
     #location
+    room_num = models.CharField(max_length = 10)
     street = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=30)
     zipcode = models.CharField(max_length=10)
-    latitude = models.CharField(max_length=10)
-    longitude = models.CharField(max_length=10)
+    latitude = models.CharField(max_length=15)
+    longitude = models.CharField(max_length=15)
