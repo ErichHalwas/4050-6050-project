73c4b6f67a1c0f3128ea446bb0c149930eae3c8d
Colton <81594541+coltonk1@users.noreply.github.com>
2025-04-17 03:25:23 -0400

    update maps page to better resemble the mockup

--- frontend/src/pages/Maps.jsx
+++ frontend/src/pages/Maps.jsx
@@ -1,7 +1,12 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import styles from "../styles/map.module.css";
+import EventCard from "../components/EventCard";
 
 function Maps() {
     const mapRef = useRef(null);
+    const [filter, setFilter] = useState("upcoming");
+    const [sortBy, setSortBy] = useState("date");
+    const [isMapHidden, setMapHidden] = useState(false);
 
     useEffect(() => {
         // Checking for geolocation
@@ -33,13 +38,75 @@ function Maps() {
     }, []);
 
     return (
-        <div
-            ref={mapRef}
-            style={{
-                width: "100%",
-                height: "100vh",
-            }}
-        ></div>
+        <>
+            <div className={styles.container}>
+                <div
+                    className={`${styles.eventDisplay} ${
+                        isMapHidden ? styles.hideMap : ""
+                    }`}
+                >
+                    <div>
+                        <div className={styles.topContainer}>
+                            <div>
+                                <p>
+                                    Searching near <b>City, State</b>
+                                </p>
+                                <div className={styles.eventControls}>
+                                    <div className={styles.controlGroup}>
+                                        <label htmlFor="filterSelect">
+                                            Filter
+                                        </label>
+                                        <select id="filterSelect">
+                                            <option value="upcoming">
+                                                Upcoming
+                                            </option>
+                                            <option value="past">Past</option>
+                                            <option value="all">All</option>
+                                        </select>
+                                    </div>
+
+                                    <div className={styles.controlGroup}>
+                                        <label htmlFor="sortSelect">
+                                            Sort by
+                                        </label>
+                                        <select id="sortSelect">
+                                            <option value="date">Date</option>
+                                            <option value="name">Name</option>
+                                            <option value="popularity">
+                                                Popularity
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <button
+                                    onClick={() => setMapHidden(!isMapHidden)}
+                                >
+                                    {isMapHidden ? "Show Map" : "Hide Map"}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.eventContainer}>
+                        <EventCard />
+                        <EventCard />
+                        <EventCard />
+                        <EventCard />
+                        <EventCard />
+                        <EventCard />
+                        <EventCard />
+                    </div>
+                </div>
+                <div
+                    ref={mapRef}
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                    }}
+                ></div>
+            </div>
+        </>
     );
 }
 
