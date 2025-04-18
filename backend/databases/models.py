231c048849f4400ee99e4a3338f7568333d46f44
Joshua <joshua@dason.org>
2025-04-18 18:31:51 -0400

    Update models.py to add default values to entry

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -7,27 +7,30 @@ class user_info(models.Model):
     password = models.CharField(max_length= 200)
     
     def __str__(self):
-        return self.email
+        return self.username
         
 class event_info(models.Model):
     #event description
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
-    host = models.CharField(max_length=100)
-    url = models.CharField(max_length=200)
-    attending = models.IntegerField(max_length = 10)
+    title = models.CharField(max_length=200,default = '')
+    description = models.CharField(max_length=500, default = '')
+    host = models.CharField(max_length=100, default = '')
+    url = models.CharField(max_length=200, default = '')
+    attending = models.IntegerField(default = 0,)
     
     #date
-    start_time = models.CharField(max_length=20)
-    end_time = models.CharField(max_length=20)
-    time_zone = models.CharField(max_length = 35)
-    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=20, default = '')
+    end_time = models.CharField(max_length=20, default = '')
+    time_zone = models.CharField(max_length = 35, default = '')
+    date = models.CharField(max_length=40, default = '')
     
     #location
-    room_num = models.CharField(max_length = 10)
-    street = models.CharField(max_length=200)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=30)
-    zipcode = models.CharField(max_length=10)
-    latitude = models.CharField(max_length=15)
-    longitude = models.CharField(max_length=15)
+    room_num = models.CharField(max_length = 10, default = '')
+    street = models.CharField(max_length=200,default = '')
+    city = models.CharField(max_length=100, default = '')
+    state = models.CharField(max_length=30, default = '')
+    zipcode = models.CharField(max_length=10, default = '')
+    latitude = models.CharField(max_length=15, default = '')
+    longitude = models.CharField(max_length=15, default = '')
+    
+    def __str__(self):
+        return self.title
