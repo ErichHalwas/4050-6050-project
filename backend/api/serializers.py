86c0f86cb84e09ca9ddb7570272b2501be34bc2d
nd3977 <99786520+nd3977@users.noreply.github.com>
2025-04-19 14:13:53 -0400

    Event creation/deletion and tests

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -11,19 +11,6 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-"""
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Note
-        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'author']
-        read_only_fields = ['created_at', 'updated_at', 'author']
-        extra_kwargs = {'author': {'read_only': True}}
-
-    def create(self, validated_data):
-        request = self.context.get('request')
-        validated_data['author'] = request.user
-        return super().create(validated_data)
-        """
 
 class EventSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", required=False)
