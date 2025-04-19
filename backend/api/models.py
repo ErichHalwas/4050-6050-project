c80460ec68711d77608dc59eb4d19435ccb43a15
KhamulTheEasterling <joshua@dason.org>
2025-04-19 13:07:33 -0400

    intial DAO api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,14 +1,99 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+import traceback
 
 # Create your models here.
-
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
-
+class user_info(models.Model):
+    username = models.CharField(max_length= 48, primary_key = True)
+    email = models.CharField(max_length= 128)
+    password = models.CharField(max_length= 64)
+    
     def __str__(self):
-        return self.title
+        return self.username + '|' + self.email + '|' + self.password
+        #return self.username
+    
+    def get_user_by_username(username):
+        return user_info.objects.get(username = username)
+    
+    def get_user_by_email(email):
+        return user_info.objects.get(email = email)
+    
+    def validate_by_username(username,password):
+        users = user_info.objects.filter(username = username, password = password)
+        if users.count() != 1:
+            print("username, password not found")
+            return False
+        
+        return True
+    
+    def delete_by_username(username):
+        rtn_flag = False
+        try:
+            if type(username) != str:
+                raise Exception("username parameter should be string")
+            users = user_info.objects.filter(username = username)
+            if users.count() == 0:
+                print("user does not exists")
+                rtn_flag = False
+            else:
+                for user in users:
+                    user.delete()
+                    print("user deleted")
+                    rtn_flag = True                
+        except Exception:
+            print(traceback.format_exc())
+            rtn_flag = False
+                
+        return rtn_flag
+    
+    def register_user(username,email,password):
+        """
+        1. validate username does not already exist
+        2. If exist return error
+        3. If does not exist, insert into database
+        """
+        rtn_flag = False
+        try:
+            if type(username) != str:
+                raise Exception("username parameter should be string")
+            users = user_info.objects.filter(username = username)
+            if users.count() > 0:
+                print("user exists")
+            else:
+                user_info.objects.create(username =username, email = email, password = password)
+                rtn_flag = True                
+        except Exception:
+            print(traceback.format_exc())
+            rtn_flag = False
+                
+        return rtn_flag
+        
+        
+class event_info(models.Model):
+    #event description
+    title = models.CharField(max_length=128,default = '')
+    description = models.CharField(max_length=512, default = '')
+    host = models.CharField(max_length=128, default = '')
+    url = models.CharField(max_length=256, default = '')
+    attendees = models.IntegerField(default = 0,)
+    
+    #date
+    start_time = models.DateTimeField(default = timezone.now, null = False)
+    end_time = models.DateTimeField(default = timezone.now, null = False)
+    time_zone = models.CharField(max_length = 64, default = 'US/Eastern')
+    
+    #location
+    street = models.CharField(max_length=256,default = '')
+    city = models.CharField(max_length=128, default = '')
+    state = models.CharField(max_length=64, default = '')
+    zipcode = models.CharField(max_length=10, default = '')
+    latitude = models.CharField(max_length=16, default = '')
+    longitude = models.CharField(max_length=16, default = '')
+    
+    def __str__(self):
+        return self.title + '|' + self.description + '|' + self.host + '|' + self.url
+        + '|' + self.attendees + '|' + self.start_time + '|' + self.end_time + '|' + self.time_zone
+        + '|' + self.street + '|' + self.city + '|' + self.state + '|' + self.zipcode + '|' + self.latitude
+        + '|' + self.longitude
+        #return self.title
+            
