024028742fc3980b8aaf00a8e645e137c60ad38d
KhamulTheEasterling <joshua@dason.org>
2025-04-19 20:09:32 -0400

    Initial release of API

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,49 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import event_info as Event
+from .models import Event_Info
 from .models import User_Info
+import re
 
 class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_Info
         fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}} #does not return password column, but can POST all columns
+    
+    def validate_email(self, value):
+        valid = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', value)
+        if not valid:
+            raise serializers.ValidationError("not a valid email")
+        return value
 
+    def create(self, validated_data):
+        user = User_Info.objects.create(**validated_data)
+        return user
+
+class EventInfoSerializer(serializers.ModelSerializer):
+    start_time = serializers.DateTimeField(format="%m/%d/%Y %I:%M:%S %p %Z", required=False)
+    end_time = serializers.DateTimeField(format="%m/%d/%Y %I:%M:%S %p %Z", required=False)
+
+    class Meta:
+        model = Event_Info
+        fields = ['id', 'title', 'description', 'host', 'url', 'attendees', 'start_time', 'end_time', 'time_zone', 'street', 'city', 'state', 'zipcode', 'latitude', 'longitude']
+    
+    def create(self, validated_data):
+        event = Event_Info.objects.create(**validated_data)
+        return event
+
+    def validate_latitude(self, value):
+        if value < -90 or value > 90:
+            raise serializers.ValidationError("Latitude must be between -90 and 90 degrees")
+        return value
+    
+    def validate_longitude(self, value):
+        if value < -180 or value > 180:
+            raise serializers.ValidationError("Longitude must be between -180 and 180 degrees")
+        return value
+    
+        
+#IS USERSERIALIZER USED
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -17,6 +53,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
 """
 class EventSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", required=False)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets,generics
-from .models import User_Info
-from .serializers import UserInfoSerializer
+from rest_framework.response import Response
+from .models import User_Info, Event_Info
+from .serializers import UserInfoSerializer, EventInfoSerializer
 
 # Create your views here.
 
@@ -9,3 +10,25 @@ from .serializers import UserInfoSerializer
 class UserInfoViewSet(viewsets.ModelViewSet):
     queryset = User_Info.objects.all()
     serializer_class = UserInfoSerializer
+    
+    def post(self, request):
+        serializer = UserInfoSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+class EventInfoViewSet(viewsets.ModelViewSet):
+    queryset = Event_Info.objects.all()
+    serializer_class = EventInfoSerializer
+    
+    def post(self, request):
+        serializer = EventInfoSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+
